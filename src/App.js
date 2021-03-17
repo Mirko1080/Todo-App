667bd741ce89e1f21459e227bfe3bcee365e35b0
Mirko Pezo <accfireover@tutanota.com>
2021-03-17 13:13:00 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import TaskList from './TaskList.js';
+import NewTask from './NewTask.js';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [nextId, setNextId] = useState(0);
+
+  const createTask = description => {
+    setTasks(tasks => ([...tasks, {id: nextId, description: description, done: false}]));
+    setNextId(nextId + 1);
+  }
+
+  const markTaskDone = id => {
+    const newTaskList = tasks.map(task => {
+      if(task.id === id) task.done = true;
+      return task;
+    });
+    setTasks(newTaskList);
+  }
+
+  const deleteTask = id => {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="bg-warning p-5">
+            <h1 className="display-4">Todo App</h1>  
+          </div>
+          <br />
+          <NewTask createTask={createTask} />
+          <br />
+          <TaskList tasks={tasks} markTaskDone={markTaskDone} deleteTask={deleteTask} />
+        </div>
+      </div>  
+    </div>
+  );
+}
+
+export default App;
